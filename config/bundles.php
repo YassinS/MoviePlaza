f91c3e8e0167259525b22fc51963ef06539a49c8
Yassin Sassi <yassin.sassi@krankikom.de>
2023-08-21 12:46:07 +0200

    added cmf

--- config/bundles.php
+++ config/bundles.php
@@ -1,9 +1,12 @@
 <?php
-
+use CustomerManagementFrameworkBundle\PimcoreCustomerManagementFrameworkBundle;
+use Pimcore\Bundle\ObjectMergerBundle\ObjectMergerBundle;
 
 return [
     //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    PimcoreCustomerManagementFrameworkBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Pimcore\Bundle\TinymceBundle\PimcoreTinymceBundle::class => ['all' => true],
-    \Pimcore\Bundle\ApplicationLoggerBundle\PimcoreApplicationLoggerBundle::class => ['all' => true],
+    Pimcore\Bundle\ApplicationLoggerBundle\PimcoreApplicationLoggerBundle::class => ['all' => true],
+    ObjectMergerBundle::class => ['all' => true],
 ];
